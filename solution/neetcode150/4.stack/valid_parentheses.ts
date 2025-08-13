b78c7f533881e42d224694f7904ae71c3e0fa62e
kiloexabyte <vothanhdat278+1@gmail.com>
2025-08-13 17:19:58 -0500

    lc 20

---
+++ solution/neetcode150/4.stack/valid_parentheses.ts
@@ -0,0 +1,25 @@
+function isValid(s: string): boolean {
+    const stack: string[] = [];
+    const dict: Map<string, string> = new Map([
+        [']', '['],
+        ['}', '{'],
+        [')', '('],
+    ]);
+
+    for (const c of s) {
+        if (dict.has(c)) {
+            if (stack.length > 0 && stack[stack.length - 1] === dict.get(c)) {
+                stack.pop();
+            } else {
+                return false;
+            }
+        } else {
+            stack.push(c);
+        }
+    }
+
+    if (stack.length === 0) return true;
+    return false
+
+
+};
